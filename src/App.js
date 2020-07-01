1fe1f302612c4094392770eb604593c5d699fe6d
ektabindal <ektabindal.india@gmail.com>
2020-07-01 14:22:22 -0700

    message

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+
 import Header from './components/Header/Header';
-import RegistrationForm from '.components/RegistrationForm/RegistrationForm';
+import React, {useState} from 'react';
+import RegistrationForm from './components/RegistrationForm/RegistrationForm';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 function App() {
-  return (
+return (
+    <Router>
     <div className="App">
-      <RegistrationForm/>
       <Header/>
-    </div>
-  )
+        <div className="container d-flex align-items-center flex-column">
+          <Switch>
+            <Route path="/" exact={true}>
+              <RegistrationForm />
+            </Route>
+          </Switch>
+       </div>
+   </div>
+  </Router>
+  )  
 }
 export default App;
